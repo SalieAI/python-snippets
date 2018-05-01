09d13766b6cabcb256ed804cdc9b38e1cd4836ba
Norbaeocystin <baranrastislav@gmail.com>
2018-05-01 10:43:20 +0200

    Create cleaner.py
    Two functions which clean strings from empty spaces on the start and on the end of string, first is using loop which breaks on non empty character, second is using list comprehension

---
+++ cleaner.py
@@ -0,0 +1,25 @@
+def clean(string):
+    '''
+    function which cleans string from empty spaces on the start and on the end of string for example:
+    input is : "   Hello World    " and output is "Hello World"
+    '''
+    first = 0
+    for item in string:
+        if item != ' ':
+            first = string.index(item)
+            break
+    string = string[::-1]
+    last = 0
+    for item in string:
+        if item != ' ':
+            last = string.index(item)
+            break
+    return string[::-1][first:len(string)-last]
+    
+ def clean2(string):
+    '''
+    function which cleans string from empty spaces on the start and on the end of string for example:
+    input is : "   Hello World    " and output is "Hello World"
+    '''
+    nonempty = [string.index(item) for item in string if item != ' ']
+    return string[nonempty[0]:nonempty[-1]+1]

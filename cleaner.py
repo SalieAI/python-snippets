f548be5819f8f363e4df222d6ad46e6613b8786f
Norbaeocystin <baranrastislav@gmail.com>
2018-05-26 22:03:48 +0200

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,3 +1,9 @@
+'''
+examples of functions which replaces spaces more than 2 with one space, two only on the end or the start of string, 
+last one replace also between spaces
+'''
+import re
+
 def clean(string):
     '''
     function which cleans string from empty spaces on the start and on the end of string for example:
@@ -16,7 +22,7 @@ def clean(string):
             break
     return string[::-1][first:len(string)-last]
     
- def clean2(string):
+def clean2(string):
     '''
     function which cleans string from empty spaces on the start and on the end of string for example:
     input is : "   Hello World    " and output is "Hello World"
@@ -24,3 +30,18 @@ def clean(string):
     nonempty = [string.index(item) for item in string if item != ' ']
     nonempty2 = [string[::-1].index(item) for item in string[::-1] if item != ' ']
     return string[nonempty[0]:len(string) -nonempty2[0]]
+
+def clean3(string):
+    '''
+    this function is using regex to clean string from rebundant spaces
+    '''
+    try:
+        for item in re.findall('[" "]{2,}',string):
+            string = string.replace(item, ' ')
+            if string[0] == ' ':
+                string = string[1:]
+            if string[-1] == ' ':
+                string = string[:-1]
+        return string
+    except:
+        return string
